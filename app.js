f49be1e5121ab9b9bcdf9ddb73350563b02fce5a
WillyAguilar09 <jwsellbycar@gmail.com>
2025-03-26 16:40:14 -0600

    agregar la Ruta en App routes

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const usuarioRoutes=require('./routes/usuarios')
 const vehiculosRoutes = require('./routes/vehiculos');
 const authRoutes =require('./routes/authUser')
 const clientesRoutes=require('./routes/clientes')
+const ventasRoutes=require('./routes/ventas')
 app.use(express.json());
 app.use('/api',usuarioRoutes)
 app.use('/api',vehiculosRoutes)
 app.use('/api',authRoutes)
 app.use('/api',clientesRoutes)
+app.use('/api',ventasRoutes)
 
 
 
